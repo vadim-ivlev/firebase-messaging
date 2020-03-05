dfadb016903afebe92a6c2b522aef8ab0d013edb
vadim-ivlev <vadim.ivlev@gmail.com>
2020-03-06 02:24:00 +0300

    .

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,53 @@
+const fetch = require('node-fetch');
 const functions = require('firebase-functions');
+const FCM_SERVER_KEY = require('./fcm-server-key-module')
+// console.log("FCM_SERVER_KEY=", FCM_SERVER_KEY)
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+ response.send("Hello from Firebase1!");
 });
+
+exports.subscribeToRGRU = functions.https.onRequest((request, response) => {  
+    const iid = request.query.iid
+    // console.log('iid=',iid)
+    if (!iid) {
+        response.status(400).send({
+            error:'No iid',
+            message:'Please provide FCM client Instance ID as a request parameter iid. iid=[instance-id]'
+        })
+        return
+    }
+
+    subscribe(iid,'rgru')
+    .then(res =>  {
+        console.log("res.status=",res.status)
+        return res.json()
+    })
+    .then(body => {
+        console.log(body)
+        response.send(body) 
+    })
+    .catch(err => {
+        console.log("ERR",err)
+        response.send(err) 
+    })
+
+});
+   
+function subscribe(whom, to) {
+    return fetch(`https://iid.googleapis.com/iid/v1:batchAdd`, {
+      'method': 'POST',
+      'headers': {
+        'Authorization': 'key=' + FCM_SERVER_KEY,
+        'Content-Type': 'application/json'
+      },
+      'body': JSON.stringify({
+          'to': '/topics/'+to,
+          'registration_tokens': [whom]
+      })
+    })
+  }
+   
--- sh/push.sh
+++ sh/push.sh
@@ -5,7 +5,8 @@ npm run build
 git add -A .
 git commit -m "."
 
-git push origin --all
 git push gitlab --all
 git push github --all
+
+git push origin --all
 
